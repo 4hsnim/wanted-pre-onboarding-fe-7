6db2a437c05f5e001b1d29b37087f19102a8a889
4hsnim <4hsnim@naver.com>
2022-10-11 01:17:14 +0900

    feature: 라우팅설정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter,
+  Outlet,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import Header from "./components/Header";
 import LoginPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
+import ToDoPage from "./pages/ToDoPage";
 import "./styles/App.css";
 
 function App() {
+  const isLoggedin = localStorage.getItem("access_token");
   return (
     <BrowserRouter className="wrapper">
       <Header />
       <Routes>
+        <Route
+          path="/"
+          element={isLoggedin ? <Navigate to="/todo" /> : <Outlet />}
+        />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/todo"
+          element={!isLoggedin ? <Navigate to="/" /> : <ToDoPage />}
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,27 @@
 import Form from "./Form";
+import axios from "axios";
 
 function Login() {
-  return <Form title="로그인" />;
+  const loginHandler = async (data) => {
+    console.log(data);
+    await axios({
+      url: "https://pre-onboarding-selection-task.shop/auth/signin",
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: data,
+    })
+      .then((response) => {
+        console.log(response.data);
+        localStorage.setItem("access_token", response.data.access_token);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  return <Form title="로그인" onSubmit={loginHandler} />;
 }
 
 export default Login;
