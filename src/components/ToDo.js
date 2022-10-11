1dfa42bc4bd2ba50f2e6b593cf6b4d5344351fc3
4hsnim <4hsnim@naver.com>
2022-10-11 13:43:35 +0900

    feature: Todo Get요청 기능구현

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "../styles/ToDo.module.css";
 
-function ToDo() {
+function ToDo(props) {
   return (
     <div className={styles.wrapper}>
       <div className={styles.todo}>
-        <span>밥먹기</span>
+        <span>{props.data.todo}</span>
       </div>
       <button className={styles.button}>완료</button>
       <button className={styles.button}>수정</button>
--- src/pages/ToDoPage.js
+++ src/pages/ToDoPage.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/ToDoPage.module.css";
 import ToDo from "../components/ToDo";
 import ToDoForm from "../components/ToDoForm";
+import axios from "axios";
 
 function ToDoPage() {
+  const [toDoList, setToDoList] = useState();
+  const access_token = localStorage.getItem("access_token");
+  const getTodoList = async () => {
+    await axios({
+      url: "https://pre-onboarding-selection-task.shop/todos",
+      method: "get",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    })
+      .then((response) => {
+        console.log(response.data);
+        setToDoList(response.data);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  useEffect(() => {
+    getTodoList();
+  }, []);
+
   return (
     <>
       <ToDoForm />
       <div className={styles.wrapper}>
         <h2>할일 목록</h2>
-
-        <ToDo />
-        <ToDo />
-        <ToDo />
-        <ToDo />
+        {toDoList && toDoList.map((todo) => <ToDo data={todo} key={todo.id} />)}
       </div>
     </>
   );
